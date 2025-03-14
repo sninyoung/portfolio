0babe31bf7ee2a11ede59c6bb08bcc53dfb42df7
sninyy <128959123+sninyoung@users.noreply.github.com>
2025-03-14 18:32:04 +0900

    포폴 코딩 끗(2)

--- js/main.js
+++ js/main.js
@@ -16,10 +16,47 @@ $(document).ready(function(){
     })
     /************************ 공통요소 :: 종료 ***********************/
 
+    /***상단메뉴 클릭하면 해당 컨텐츠로 이동**/
+    let menuName = $('.header')  // 상단에 고정할 메뉴 영역 선택자
+    let menuItem = $('.header ul li') // data-link 값을 준 클릭할 요소의 선택자
+    let sectionName
+    let moveTop
+    let areaTop
+    let areaH
+    let areaName
+    let scrollTop
+    menuItem.on('click', function(){
+        sectionName = $(this).attr('data-link')
+        moveTop = $('*[data-menu="'+sectionName+'"]').offset().top - menuName.height()
+        $('html, body').animate({
+            scrollTop : moveTop
+        }, 500)
+    })
+    menuChk()
+    $(window).scroll(function(){
+        menuChk()
+    })
+    function menuChk(){
+        scrollTop = $(window).scrollTop()
+        $.each($('*[data-menu]'), function(idx, item){
+            areaTop = $('*[data-menu]').eq(idx).offset().top
+            areaH = $('*[data-menu]').eq(idx).height()
+            areaName = $('*[data-menu]').eq(idx).attr('data-menu')
+            if((scrollTop >= areaTop - menuName.height()*1.2) && (scrollTop < areaTop + areaH - menuName.height())){
+                menuItem.removeClass('active')
+                menuItem.siblings('[data-link="'+areaName+'"]').addClass('active')
+            }else if(scrollTop < $('*[data-menu]').first().offset().top){
+                menuItem.removeClass('active')
+            }else if(scrollTop > $('*[data-menu]').last().offset().top + $('*[data-menu]').last().height()){
+                menuItem.removeClass('active')
+            }
+        });
+    }
 
     gsap.from(".portfolio .writing span", {
         duration: 4, 
         text: ""
-    }) //글자를 감싸는 요소
+    }) //글자 타이핑하는 effect
 
 })//$(document).ready
+
