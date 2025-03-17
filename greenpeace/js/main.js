724f0e6913563c7c22e3668ad94ac4bcd538d1f3
sninyy <128959123+sninyoung@users.noreply.github.com>
2025-03-17 11:00:47 +0900

    그린피스 핵심가치 애니메이션 추가

--- greenpeace/js/main.js
+++ greenpeace/js/main.js
@@ -188,5 +188,11 @@ $(document).ready(function(){
     /*************************footer intl_site 열기 open (끝)  **************************/
 
 
+     /* aos 애니메이션 */
+     AOS.init({
+        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 500, // 애니메이션 효과가 작동되는 시간
+        easing: 'ease', // 가속도
+    });
 
 })//$(document).ready 
